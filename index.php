3fdf99328d67b24042b0499764cce14a8e357d4b
Shuichi Nagao <ngo38114@gmail.com>
2017-01-10 14:01:47 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
-	$id1 = 297;
-    // id1 = 297だと進撃の巨人
-    $id2 = 88;
-    // id = 88だと12月に発売されたヤンジャンの掲載分
+    $comic_id = 297;
+    // comic_id = 297だと進撃の巨人
+    $story_id = 88;
+    // story_id = 88だと12月に発売されたヤンジャンの掲載分
     require_once("phpQuery-onefile.php");
     $results = array();
     $page = 1;
     while(1 == 1){
-        $url =  'http://mangamatome.net/manga/'.$id1.'/'.$id2.'/'.$page;
+        $url =  'http://mangamatome.net/manga/'.$comic_id.'/'.$story_id.'/'.$page;
         $html = file_get_contents($url);
         $doc = phpQuery::newDocument($html);
         if (!$doc["#main-image"] -> attr('src')) break;
         $results[] = $doc["#main-image"] -> attr('src');
         $page += 1;
-        if ($page==100) {
+        if ($page == 100) {
             //var_dump("loop");
             break;
         }
